2f168d296c027c9ade3b415ada1dec19ec6da7e4
unknown <chandresh.02vikram@gmail.com>
2020-10-13 12:32:36 +0530

    added git learning project to local repository

---
+++ src/main/java/com/onpassive/chandresh/git_learning/App.java
@@ -0,0 +1,19 @@
+package com.onpassive.chandresh.git_learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	
+	public void addData(String data)
+	{
+		System.out.println("data is::"+data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
