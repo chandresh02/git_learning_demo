97b7795637ddcd5cc9f18245e95ed80184ab7dbb
unknown <chandresh.02vikram@gmail.com>
2020-10-13 15:26:18 +0530

    my first change

--- src/main/java/com/onpassive/chandresh/git_learning/App.java
+++ src/main/java/com/onpassive/chandresh/git_learning/App.java
@@ -11,6 +11,10 @@ public class App
 	public void addData(String data)
 	{
 		System.out.println("data is::"+data);
+		if(data.equals("chandresh vikram"))
+		{
+			System.out.println("welcome");
+		}
 	}
     public static void main( String[] args )
     {
